480ce4d729d5301c1618a18ec9f83872ce1c6e39
Kingsley Quarshie <kingsley.quarshie.01@gmail.com>
2026-09-10 11:41:07 +0000

    Final Commit

--- insurance/models.py
+++ insurance/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from customer.models import Customer
 class Category(models.Model):
     category_name =models.CharField(max_length=20)
-    creation_date =models.DateField(auto_now=True)
+    creation_date =models.DateField(auto_now=True) 
     def __str__(self):
         return self.category_name
 

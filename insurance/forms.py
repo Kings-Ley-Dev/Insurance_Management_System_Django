00775a2557b236d2a2fc721bb223efde2ad9a5d5
Kingsley Quarshie <kingsley.quarshie.01@gmail.com>
2026-09-10 11:41:36 +0000

    Final Commit

--- insurance/forms.py
+++ insurance/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from . import models
+from . import models 
 
 class ContactusForm(forms.Form):
     Name = forms.CharField(max_length=30)

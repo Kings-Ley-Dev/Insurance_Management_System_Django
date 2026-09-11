2005445aef6c94a27123fa80403cb2a226af7003
Kingsley Quarshie <kingsley.quarshie.01@gmail.com>
2026-09-11 22:06:44 +0000

    Final Commit

--- customer/urls.py
+++ customer/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView 
 
 urlpatterns = [
     path('customerclick', views.customerclick_view,name='customerclick'),
